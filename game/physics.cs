f2579b5390e477fc37ed80653f811a65a94317d2
VenomFeed <93274199+vatari4@users.noreply.github.com>
2023-01-28 21:23:27 +0500

    Update physics.cs

--- game/physics.cs
+++ game/physics.cs
@@ -1,50 +1,92 @@
+ using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
 public class Character : MonoBehaviour
 {
-[SerializeField]
-private float speed = 3.0F;
-[SerializeField]
-private float jumpForce = 10.0F;
+    [SerializeField]
+    private float speed = 3.0F;
+    [SerializeField]
+    private float jumpForce = 10.0F;
 
-private bool isGrounded = false;
+    private bool isGrounded = false;
 
-private Rigidbody2D rb;
-private SpriteRenderer sprite;
+    private Rigidbody2D rb;
+    private Animator anim;
+    private SpriteRenderer sprite;
 
 
-private void Awake() 
+    private void Awake() 
+    {
+        rb = GetComponent<Rigidbody2D>();
+        sprite = GetComponentInChildren<SpriteRenderer>();
+        anim = GetComponent<Animator>();
+    }
+
+    private void Jump()
+    {
+        rb.AddForce(transform.up * jumpForce, ForceMode2D.Impulse);
+    }
+
+
+    private void FixedUpdate()
+    {
+        CheckGround();
+    }
+
+    void Update()
+    {
+        if (isGrounded) State = States.idle;
+        if (Input.GetButton("Horizontal")) Run();
+        if (isGrounded && Input.GetButtonDown("Jump")) Jump();
+    }
+    private void Run()
+    {
+        if (isGrounded) State = States.run;
+
+        Vector3 dir = transform.right * Input.GetAxis("Horizontal");
+        transform.position = Vector3.MoveTowards(transform.position, transform.position + dir, speed * Time.deltaTime);
+        sprite.flipX = dir.x < 0.0f;
+        
+    }
+    private void CheckGround()
+    {
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position, 0.3F);
+        isGrounded = colliders.Length > 1;
+
+        if (!isGrounded) State = States.jump;
+    }
+
+    private States State
+    {
+        get { return (States)anim.GetInteger("state"); }
+        set { anim.SetInteger("state", (int)value); }
+    } 
+}
+public class CameraConroller : MonoBehaviour
 {
-    rb = GetComponent<Rigidbody2D>();
-    sprite = GetComponentInChildren<SpriteRenderer>();
-}
+    [SerializeField]
+    private Transform player;
+    private Vector3 pos;
 
-private void Jump()
-{
-    rb.AddForce(transform.up * jumpForce, ForceMode2D.Impulse);
-}
+    private void Awake() 
+    {
+        if (!player)
+            player = FindObjectOfType<Character>().tranform;
 
+    }
+    private void Update() 
+    {
+        pos = player.position;
 
-private void FixedUpdate()
-{
-    CheckGround();
-}
-
-void Update()
-{
-    if (Input.GetButton("Horizontal")) Run();
-    if (isGrounded && Input.GetButtonDown("Jump")) Jump();
-}
-private void Run()
-{
-    Vector3 dir = transform.right * Input.GetAxis("Horizontal");
-    transform.position = Vector3.MoveTowards(transform.position, transform.position + dir, speed * Time.deltaTime);
-    sprite.flipX = dir.x < 0.0f;
+            transform.position = Vector3.Lerp(transform.position, pos, Time.deltaTime); //lerp - delaet dvijenie plavnim
+    }
 
 }
-private void CheckGround()
-{
-    Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position, 0.3F);
-    isGrounded = colliders.Length > 1;
-}
-}
 
+public enum States 
+{ 
+    idle,
+    run,
+    jump
+}
